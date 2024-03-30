ec981c4f8a4c52fa9c37f5451c16948297b87290
Jake <56329025+jakeo-dev@users.noreply.github.com>
2024-03-30 07:48:57 -0700

    First

---
+++ main.py
@@ -0,0 +1,26 @@
+numColleges = int(input("\nHow many colleges are you applying to? "))
+
+accRates = []
+
+pAll = 1
+pNone = 1
+pOne = 1
+
+for i in range(numColleges):
+    accRates.append(float(input("% (0-100) acceptance rate of college #" + str(i + 1) + ": ")) / 100)
+
+print("\n")
+
+# calculate P(all)
+for acc in accRates:
+    pAll *= acc
+print("Probability of getting accepted into EVERY college: " + str(round(pAll * 100, 6)) + "%")
+
+# calculate P(none)
+for acc in accRates:
+    pNone *= (1 - acc)
+print("Probability of getting accepted into NONE of the colleges: " + str(round(pNone * 100, 6)) + "%")
+
+# calculate P(one)
+pOne = 1 - pNone
+print("Probability of getting accepted into ONE of the colleges: " + str(round(pOne * 100, 6)) + "%")
